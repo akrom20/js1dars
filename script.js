702bc11b7dee49d44535241a1fce880fd7b311d3
Akromjon <abduvohidovakrom01@gmail.com>
2021-11-08 11:57:59 +0300

    js1dars

---
+++ script.js
@@ -0,0 +1,60 @@
+var name = "akromjon"
+var name = 20
+var boolean = true
+
+var str = prompt("ismingizni kiriting")
+console.log('sizning ismingiz: ' + str);
+
+var year = + prompt ('yoshingizni kiriting!')
+console.log('men '+ year + ' yoshdaman' );
+
+var misol1 =(6+3)
+var misol2 =(10*2)
+var misol3 =(10%3)
+var misol4 =(20-9)
+var misol5 =(20+35)
+var misol6 =(55/10)
+var misol7 =(10*3)
+var misol8 =(25-9)
+var misol9 =(47-12)
+var misol10 =(43*2)
+var misol11 =(50+50)
+var misol12 =(510-25)
+var misol13 =(2+5)
+
+
+
+
+
+
+var num1 = + prompt('6+3= ?' )
+console.log('Misol 1:' +' 6+3='+ misol1+' sizning javob '+num1 );
+var num2 = + prompt('10*2= ?' )
+console.log('Misol 2:' +' 10*2='+misol2+ ' sizning javob '+num2 );
+var num3 = + prompt('10%3= ?' )
+console.log('Misol 3:' +' 10%3='+misol3 +' sizning javob '+num3 );
+var num4 = + prompt('20-9= ?' )
+console.log('Misol 4:' +' 20-9=' + misol4+' sizning javob '+num4);
+var num5 = + prompt('20+35= ?' )
+console.log('Misol 5:' +' 20+35='+ misol5+' sizning javob '+num5 );
+var num6 = + prompt('55/10= ?' )
+console.log('Misol 6:' +' 55-10=' +misol6+' sizning javob '+num6 );
+var num7 = + prompt('10*3= ?' )
+console.log('Misol 7:' +' 10*3=' +misol7+' sizning javob '+num7 );
+var num8 = + prompt('25-9= ?' )
+console.log('Misol 8:' +' 25-9='+ misol8+' sizning javob '+num8 );
+var num9 = + prompt('47-12= ?' )
+console.log('Misol 9:' +' 47-12='+misol9+' sizning javob '+num9 );
+var num10 = + prompt('43*2= ?' )
+console.log('Misol 10:' +' 43*2='+misol10+' sizning javob '+num10 );
+var num11 = + prompt('50+50= ?' )
+console.log('Misol 11:' +' 50+50=' +misol11+' sizning javob '+num11 );
+var num12 = + prompt('510-25= ?' )
+console.log('Misol 12:' +' 510-25=' +misol12+' sizning javob '+num12 );
+var num13 = + prompt('2+5= ?' )
+console.log('Misol 13:' +' 2+5='+misol13 +' sizning javob '+num13 );
+
+
+
+
+alert('Natijani korish')
